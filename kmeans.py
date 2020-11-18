0de600b026f667eb975379f7ee7e8814272acb2c
Geoffrey Corvera <geoffcorvera@gmail.com>
2020-11-18 09:36:10 -0800

    Assignment function TODO: initialize k mus

--- kmeans.py
+++ kmeans.py
@@ -7,10 +7,18 @@ except (IndexError):
     K = 2
 
 class KMeans(object):
-    def __init__(self,k):
+    def __init__(self,k,X):
         self.k = k
-        self.ksets = [set() for _ in range(K)]
+        # TODO: randomly initialize centroids (shift by X.mean, scale by X.std)
         self.centroids = list()
+
+    def assign(self, X):
+        self.clusters = [set() for _ in range(self.k)]
+        for x in X:
+            l2distance = [x - m for m in self.centroids]
+            nearest = np.argmin([np.linalg.norm(d) for d in l2distance])
+            self.clusters[nearest].add(x)
+            self.centroids[nearest] = np.mean(self.clusters[nearest])
 
     def train(self):
         self.centroids = [random.random() for _ in range(self.k)]
@@ -22,7 +30,15 @@ class KMeans(object):
         return np.argmin(np.array(wcss))
 
 nft = 4
-km = KMeans(K)
+X = np.ones((5,3))
+km = KMeans(K, X)
 km.train()
 datum = [random.random() for _ in range(nft)]
 print(km.classify(datum))
+
+# E step (assign Z clusters)
+    # find most plausible assignments for Z (sub-optimzation prob)
+# MLE estimates (maximize likelihood of the paramters)
+    # (MLE)est.params
+    # k means parameters: cluster centroids (mus)
+# repeat E & MLE]
