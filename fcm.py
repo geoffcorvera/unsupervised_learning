8558f9e805f2b630152788a7ca3ab24d7088bbfa
Geoffrey Corvera <geoffcorvera@gmail.com>
2020-11-28 17:45:24 -0800

    initialize fcm params, centroids, and memberships

--- fcm.py
+++ fcm.py
@@ -3,12 +3,18 @@ import numpy as np
 class FCM(object):
 
     def __init__(self,c,m,X):
-        # initialize memberships XXX each row should sum to 1
-        memberships = None
+        # initialize memberships, & scale so rows sum to 1
+        memberships = np.random.rand(X.shape[0], c)
+        sums = np.sum(memberships, axis=1)
+        memberships = memberships/sums[:,np.newaxis]
+        
+        # XXX: Should initial centroids be scaled and shifted?
+        centroids = np.random.rand(c, X.shape[1])
 
         self.c = c
         self.m = m
         self.memberships = memberships
+        self.centroids = centroids
     
     def nextCentroid(self):
         pass
