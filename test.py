a4769c3ecac4144866a918abfdfb59218f3de84f
Geoffrey Corvera <geoffcorvera@gmail.com>
2020-11-29 13:56:55 -0800

    refactor plotting in test.py

--- test.py
+++ test.py
@@ -6,10 +6,6 @@ from fcm import FCM
 colors = np.array(['#49111c','#ee2e31','#1d7874','#7f7f7f'])
 data = np.genfromtxt('data/545_cluster_dataset.txt')
 
-# TODO: run 10 trials and select model with lowest SSE
-def runTrials(ntrials=10):
-    pass
-
 def plotGroundTruth(X):
     c1 = X[:500]
     c2 = X[500:1000]
@@ -18,6 +14,19 @@ def plotGroundTruth(X):
     for i,cluster in enumerate(clusters):
         plt.scatter(cluster[:,:1], cluster[:,1:], c=colors[i])
     plt.show()
+
+
+def plotKClusters(results,k,X):
+    clusters = [X[np.where(results==i)] for i in range(k)]
+    for cluster,color in zip(clusters,colors[:k]):
+        plt.scatter(cluster[:,:1], cluster[:,1:], c=color)
+    plt.show()
+
+
+# TODO: run 10 trials and select model with lowest SSE
+def runTrials(ntrials=10):
+    pass
+
 
 def generateTestData():
     cluster1 = np.random.normal(2, 0.3, size=(5,2))
@@ -43,17 +52,12 @@ def fcm_test(K):
     err_plot = fcm.train(data, 40)
     results = fcm.classify()
 
-    # Split test data by results
-    splits = [data[np.where(results==k)] for k in range(K)]
-    
     # Plot sum-of-squares error per epoch
     plt.plot(err_plot)
     plt.show()
 
-    # Plot results
-    for cluster,color in zip(splits,colors[:K]):
-        plt.scatter(cluster[:,:1],cluster[:,1:], c=color)
-    plt.show()
+    # Plot final cluster assignments
+    plotKClusters(results,K,data)
 
 # Run test
 fcm_test(K=3)
