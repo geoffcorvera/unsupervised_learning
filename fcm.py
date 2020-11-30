fix sum of squares err calculation

fcm.py
@@ -58,7 +58,7 @@ class FCM(object):
         err = 0
         for c in range(self.c):
             cluster = X[np.where(results==c)]
-            err += np.sum(np.linalg.norm(cluster-self.centroids[c], axis=1)**2)
+            err += np.sum(np.linalg.norm(cluster-self.centroids[c], axis=1))
 
         return err
             

kmeans.py
@@ -18,7 +18,7 @@ class KMeans(object):
         err = 0
         for k in range(self.k):
             cluster = X[np.where(results==k)]
-            err += np.sum(np.linalg.norm(cluster-self.centroids[k], axis=1)**2)
+            err += np.sum(np.linalg.norm(cluster-self.centroids[k], axis=1))
         return err
 
     # E step (assign most plausible cluster to xs)